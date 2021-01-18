154b669d30aaa243c157903d347f201caaea61f5
Yuki Kawano <yukijukisfc@gmail.com>
2021-01-18 22:13:49 +0900

    api routes added

--- applog_web/__init__.py
+++ applog_web/__init__.py
@@ -7,4 +7,4 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="./applog_web/static"), name="static")
 templates = Jinja2Templates(directory="./applog_web/templates")
 
-from applog_web.views import main, tasks
+from applog_web.views import get, post
